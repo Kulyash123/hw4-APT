286421c5874f980adece742868534838d8be21fb
Your Name <kulyash.orazbekova@ce.sdu.edu.kz>
2015-03-09 18:04:30 +0600

    link the urls in urls.py

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from post.api import Post, Comment
+
+post_res = PostResource()
+comment_res = CommentResource()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^api/', include(post_res.urls)),
+  url(r'^api/', include(comment_res.urls)),.urls)),
 )
